7ed3276fc159fbf8557999dd1b516b3c72ade710
alr11118 <alara.engin2008@gmail.com>
2025-11-25 19:42:34 -0800

    fixed task 2

--- Shapes.java
+++ Shapes.java
@@ -3,7 +3,7 @@ public class Shapes {
         Turtle myTurtle = new Turtle();
         myTurtle.up();
         // Task 1: Draw a regular shape
-        int numOfSides = 5;
+        int numOfSides = 3;
         myTurtle.setPosition(-200, 200);
         
         myTurtle.down();
@@ -18,13 +18,12 @@ public class Shapes {
         myTurtle.setPosition(50, 200);
         myTurtle.down();
         
-        for(int i = 0; i<numOfSides; i++){
+        for(int i = 0; i<10; i++){
          for(int j = 0; j<numOfSides; j++){
-               myTurtle.forward(20);
+               myTurtle.forward(50);
                myTurtle.right(360/numOfSides);
          }
-         myTurtle.forward(100);
-         myTurtle.right(360/numOfSides);
+         myTurtle.right(36);
         }
 
 
